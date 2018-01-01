cdf07015df45b335be787a89454c2c1dbd3493c7
CeceSoudaly <ccsoudal@yahoo.com>
2017-12-31 23:55:36 -0600

    working progress

--- WeatherApp/API/SelectCityState.swift
+++ WeatherApp/API/SelectCityState.swift
@@ -17,12 +17,12 @@ class SelectCityState: UIViewController,UIPickerViewDataSource, UIPickerViewDele
     
     @IBOutlet weak var SelectedCity: UILabel!
     
-    var CityList = [ "AL Montgomery","AK Juneau","AZ Phoenix","AR Little Rock","CA Sacramento","CA San Francisco","CA Los Angeles","CO Denver","CT Hartford", "DE Dover", "FL Tallahassee","FL Miami","FL Orlando","GA Atlanta","HI Honolulu","ID Boise","IL Springfield","IN Indianapolis","IA Des Moines","KS Topeka","KY Frankfort","LA Baton Rouge","ME Augusta","MD Annapolis","MA Boston","MI Lansing", "MN Saint Paul", "MS Jackson", "MO Jefferson City",
-         "MT Helena","NE Lincoln","NV Carson City", "NH Concord", "NJ Trenton","NM Santa Fe","NY Albany",
-          "NC Raleigh","ND Bismarck","OH Columbus", "OK Oklahoma City","OR Salem","PA Harrisburg",
-           "RI Providence","SC Columbia","SD Pierre",
-           "TN Nashville", "TX Austin", "UT Salt Lake City", "VT Montpelier", "VA Richmond", "WA Olympia",
-          "WV Charleston", "WI Madison","WY Cheyenne"]
+    var CityList = [ "AL Montgomery","AK Juneau","AZ Phoenix","AR Little Rock","CA Sacramento","CA San Francisco","CA Los Angeles","CO Denver","CT Hartford", "DE Dover", "FL Tallahassee","FL Miami","FL Orlando","GA Atlanta","HI Honolulu","ID Boise","IL Springfield","IN Indianapolis","IA Des Moines","KS Topeka","KY Frankfort",
+        "LA Baton Rouge","ME Augusta","MD Annapolis","MA Boston","MI Lansing", "MN Saint Paul", "MS Jackson",
+        "MO Jefferson City","MT Helena","NE Lincoln","NV Carson City", "NH Concord", "NJ Trenton","NM Santa Fe","NY Albany",
+        "NC Raleigh","ND Bismarck","OH Columbus", "OK Oklahoma City","OR Salem","PA Harrisburg", "RI Providence",
+        "SC Columbia","SD Pierre","TN Nashville", "TX Austin", "UT Salt Lake City", "VT Montpelier", "VA Richmond",
+        "WA Olympia", "WV Charleston", "WI Madison","WY Cheyenne"]
     
     override func viewDidLoad() {
      
@@ -48,6 +48,24 @@ class SelectCityState: UIViewController,UIPickerViewDataSource, UIPickerViewDele
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         SelectedCity.text = self.CityList[row]
+        var fullName: String = self.CityList[row]
+        let fullNameArr = fullName.components(separatedBy: " ")
+        print(fullNameArr.count)
+        let state  = fullNameArr[0]
+        print(state)
+        
+        
+        if(fullNameArr.count > 2)
+        {
+             let city = fullNameArr[1] + " " + fullNameArr[2]
+             print(city)
+        }else{
+            
+            let city = fullNameArr[1]
+            print(city)
+        }
+ 
+        
     }
     
    
