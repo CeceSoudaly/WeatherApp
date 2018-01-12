391d0f9f669f292ce224ce45385ea3e312722eb1
CeceSoudaly <ccsoudal@yahoo.com>
2018-01-11 20:51:59 -0600

    working progress.

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyCi9T8T4PbvlfIPZVQK3StCBXOneDBK4OY")
+        GMSPlacesClient.provideAPIKey("AIzaSyBamx43swr1VzAt3Hf0Cd_peaMPvRBE76k")
+    
         return true
     }
 
--- WeatherApp/View/AutoCompleteCity.swift
+++ WeatherApp/View/AutoCompleteCity.swift
@@ -7,48 +7,83 @@
 //
 
 import UIKit
+import GoogleMaps
 import GooglePlaces
 
-class  AutoCompleteCity: UIViewController , UISearchBarDelegate ,  GMSAutocompleteFetcherDelegate {
-    
-    var resultsViewController: GMSAutocompleteResultsViewController?
-    var searchController: UISearchController?
-    var resultView: UITextView?
-    
-
-    @IBAction func beginSearch(_ sender: Any) {
-        
-        
+class  AutoCompleteCity:  UIViewController , UISearchBarDelegate , LocateOnTheMap,GMSAutocompleteFetcherDelegate  {
+    func locateWithLongitude(_ lon: Double, andLatitude lat: Double, andTitle title: String) {
+       // <#code#>
     }
     
     func didAutocomplete(with predictions: [GMSAutocompletePrediction]) {
         //<#code#>
+        
+        for prediction in predictions {
+            
+            if let prediction = prediction as GMSAutocompletePrediction!{
+                self.resultsArray.append(prediction.attributedFullText.string)
+            }
+        }
+        self.searchResultController.reloadDataWithArray(self.resultsArray)
+       
+        print(resultsArray)
     }
     
     func didFailAutocompleteWithError(_ error: Error) {
         //<#code#>
     }
+    
+    
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+//    var resultView: UITextView?
+    var searchResultController: SearchResultsController!
+    var resultsArray = [String]()
+//    var gmsFetcher: GMSAutocompleteFetcher!
+ 
+    @IBAction func searchCity(_ sender: Any) {
+        
+        print("Blah Blah Seach City")
+//        resultsViewController = GMSAutocompleteResultsViewController()
+//        resultsViewController?.delegate = self
+//        let searchController = UISearchController(searchResultsController: resultsViewController)
+//
+//        searchController.searchBar.delegate = self
+//
+//        self.present(searchController, animated:true, completion: nil)
+//
+        let searchController = UISearchController(searchResultsController: searchResultController)
+
+        searchController.searchBar.delegate = self
+
+
+
+        self.present(searchController, animated:true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         resultsViewController = GMSAutocompleteResultsViewController()
         resultsViewController?.delegate = self
-        
+
         searchController = UISearchController(searchResultsController: resultsViewController)
         searchController?.searchResultsUpdater = resultsViewController
-        
+
         let subView = UIView(frame: CGRect(x: 0, y: 65.0, width: 350.0, height: 45.0))
-        
+
         subView.addSubview((searchController?.searchBar)!)
         view.addSubview(subView)
         searchController?.searchBar.sizeToFit()
         searchController?.hidesNavigationBarDuringPresentation = false
-        
+
         // When UISearchController presents the results view, present it in
         // this view controller, not one further up the chain.
         definesPresentationContext = true
     }
 }
+
 
 // Handle the user's selection.
 extension  AutoCompleteCity: GMSAutocompleteResultsViewControllerDelegate {
